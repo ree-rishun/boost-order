1355eae7b75f1af0d4cd72e0d91310fc0044bb31
ree-rishun <ree0432@gmail.com>
2021-03-16 23:51:21 +0900

    デザインを最低限作成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>TEAM SHUFFLE</h1>
     <Result msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="hello">
     <label>
-      チーム数：
       <input
         type="number"
         value="2"
         min="2"
         v-model="team_num">
+      TEAMS
     </label>
     <label
       v-for="index in Number(team_num)"
       :key="index">
-      {{ index }} :
+      {{ index }}
       <input
         v-model="teams[index - 1]"
         type="text">
     </label>
     <button
+      class="plus_button"
       @click="plus()">
       +
     </button>
     <button
+      class="shuffle_button"
       @click="shuffle()">
       shuffle
     </button>
@@ -38,15 +40,12 @@ export default {
   },
   methods: {
     shuffle () {
-
-      console.log('チーム数：' + this.teams.length);
       for (let i = 0; i < this.teams.length; i++) {
         const r = Math.floor(Math.random() % (i + 1));
         const tmp = this.teams[i];
         this.$set(this.teams, i, this.teams[r]);
         this.$set(this.teams, r, tmp);
       }
-      console.log(this.teams);
     },
     plus () {
       this.team_num++;
@@ -59,5 +58,49 @@ export default {
   label {
     display: block;
     margin: 20px auto;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  input {
+    border: none;
+    border-bottom: solid 2px #2c3e50;
+  }
+
+  input[type=number] {
+    width: 100px;
+    text-align: center;
+  }
+
+  .plus_button {
+    margin: 10px auto 30px;
+    outline: none;
+    border: none;
+    width: 30px;
+    height: 30px;
+    font-size: 25px;
+    font-weight: bold;
+    border-radius: 30px;
+    color: #ffffff;
+    background-color: #2c3e50;
+    cursor: pointer;
+    display: block;
+  }
+
+  .shuffle_button {
+    margin: 10px auto 30px;
+    outline: none;
+    border: none;
+    display: block;
+    width: 100px;
+    height: 30px;
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 30px;
+    color: #ffffff;
+    background-color: #2c3e50;
+    cursor: pointer;
   }
 </style>
